4dbeaf364be63b49c5a0758fb7607c4b334d55aa
yongchaeyoung <happy7yong83@gmail.com>
2024-07-31 14:42:13 +0900

    Changes 2

--- components/Schedule/ScheduleScreen.tsx
+++ components/Schedule/ScheduleScreen.tsx
@@ -2,14 +2,6 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import { styles } from './ScheduleScreen-styles';
 
-// 현재 날짜와 시간 가져오기
-const currentDate = new Date();
-
-// 월 추출 (0부터 11까지의 값이므로 1을 더합니다)
-const currentMonth = currentDate.getMonth() + 1;
-
-console.log(`현재 월: ${currentMonth}`);
-
 const ScheduleScreen: React.FC = () => (
   <View style={styles.container}>
     <View style={styles.ScheduleHeader}>
