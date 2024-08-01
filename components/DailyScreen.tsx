76259477ec138c584af7a28ac2756ec14ec69e1f
yongchaeyoung <happy7yong83@gmail.com>
2024-08-02 00:46:48 +0900

    feat : 일상탭에서 앨범을 선택하면 target으로 들어가게 됨.

--- components/DailyScreen.tsx
+++ components/DailyScreen.tsx
@@ -1,5 +1,7 @@
+// components/DailyScreen.tsx
 import React, { useState, useRef } from 'react';
-import { View, Image, FlatList, Dimensions, Animated, Text, ScrollView } from 'react-native';
+import { View, Image, FlatList, Dimensions, Animated, Text, ScrollView, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { styles } from './DailyScreen-styles';
 
 const images = [
@@ -13,6 +15,7 @@ const { width } = Dimensions.get('window');
 const DailyScreen: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const animatedValue = useRef(new Animated.Value(0)).current;
+  const navigation = useNavigation();
 
   const onScroll = (event: any) => {
     const index = Math.round(event.nativeEvent.contentOffset.x / width);
@@ -26,7 +29,6 @@ const DailyScreen: React.FC = () => {
 
   const renderIndicators = () => {
     return (
-
       <View style={styles.indicatorContainer}>
         {images.map((_, index) => {
           const indicatorWidth = animatedValue.interpolate({
@@ -52,79 +54,77 @@ const DailyScreen: React.FC = () => {
     );
   };
 
+  const handlePress = () => {
+    navigation.navigate('Target');  // 'Target' 화면으로 이동
+  };
+
   return (
-    <ScrollView contentContainerStyle={styles.scrollViewContainer}>
-      <View style={styles.flatListContainer}>
-        <FlatList
-          data={images}
-          horizontal
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-          onScroll={onScroll}
-          renderItem={({ item }) => (
-            <View style={[styles.imageContainer, { width }]}>
-              <Image source={item} style={styles.image} />
-            </View>
-          )}
-          keyExtractor={(item, index) => index.toString()}
-        />
-
-        {renderIndicators()}
-
-      </View>
-      <View style={styles.dailyInner}>
-        <View style={styles.innerBar}></View>
-            <View style={styles.dailyContainer}>
-              <Image source={require('../assets/png/Sunflower-image.png')}
-              style={styles.dailyContentImage} />
-              <View style={styles.rightContainer}>
-                <Text style={styles.TimeText}>AM 10:00</Text>
-                <Text style={styles.thinkText1}>해바라기를 선물로</Text><Text style={styles.thinkText2}>받아봤으면 했어요.</Text>
-                <View style={styles.line}></View>
-                <View style={styles.distance}>
-                    <Image source={require('../assets/png/distance.png')}
-                                  style={styles.distanceImage} />
-                        <Text style={styles.GPSText}>신곡동 53-52</Text>
+    <TouchableOpacity onPress={handlePress} style={styles.scrollViewContainer}>
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+        <View style={styles.flatListContainer}>
+          <FlatList
+            data={images}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            onScroll={onScroll}
+            renderItem={({ item }) => (
+              <View style={[styles.imageContainer, { width }]}>
+                <Image source={item} style={styles.image} />
+              </View>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+          />
+          {renderIndicators()}
+        </View>
+        <View style={styles.dailyInner}>
+          <View style={styles.innerBar}></View>
+          <View style={styles.dailyContainer}>
+            <Image source={require('../assets/png/Sunflower-image.png')} style={styles.dailyContentImage} />
+            <View style={styles.rightContainer}>
+              <Text style={styles.TimeText}>AM 10:00</Text>
+              <Text style={styles.thinkText1}>해바라기를 선물로</Text>
+              <Text style={styles.thinkText2}>받아봤으면 했어요.</Text>
+              <View style={styles.line}></View>
+              <View style={styles.distance}>
+                <Image source={require('../assets/png/distance.png')} style={styles.distanceImage} />
+                <Text style={styles.GPSText}>신곡동 53-52</Text>
+              </View>
+              <View style={styles.btnInner}>
+                <View style={styles.soundContainer}>
+                  <Image source={require('../assets/png/sound.png')} style={styles.SIcon} />
                 </View>
-                <View style={styles.btnInner}>
-                    <View style={styles.soundContainer}>
-                        <Image source={require('../assets/png/sound.png')}
-                          style={styles.SIcon} />
-                    </View>
-                    <View style={styles.recorderContainer}>
-                        <Image source={require('../assets/png/recorder.png')}
-                          style={styles.RIcon} />
-                    </View>
-                 </View>
+                <View style={styles.recorderContainer}>
+                  <Image source={require('../assets/png/recorder.png')} style={styles.RIcon} />
+                </View>
               </View>
             </View>
-        <View style={styles.dailyContainer}>
-                      <Image source={require('../assets/png/Sunflower-image.png')}
-                      style={styles.dailyContentImage} />
-                      <View style={styles.rightContainer}>
-                        <Text style={styles.TimeText}>AM 10:00</Text>
-                        <Text style={styles.thinkText1}>해바라기를 선물로</Text><Text style={styles.thinkText2}>받아봤으면 했어요.</Text>
-                        <View style={styles.line}></View>
-                        <View style={styles.distance}>
-                            <Image source={require('../assets/png/distance.png')}
-                                          style={styles.distanceImage} />
-                                <Text style={styles.GPSText}>신곡동 53-52</Text>
-                        </View>
-                        <View style={styles.btnInner}>
-                            <View style={styles.soundContainer}>
-                                <Image source={require('../assets/png/sound.png')}
-                                  style={styles.SIcon} />
-                            </View>
-                            <View style={styles.recorderContainer}>
-                                <Image source={require('../assets/png/recorder.png')}
-                                  style={styles.RIcon} />
-                            </View>
-                         </View>
-                      </View>
-                    </View>
-                <View style={styles.emptyContainer}></View>
-      </View>
-    </ScrollView>
+          </View>
+          <View style={styles.dailyContainer}>
+            <Image source={require('../assets/png/Sunflower-image.png')} style={styles.dailyContentImage} />
+            <View style={styles.rightContainer}>
+              <Text style={styles.TimeText}>AM 10:00</Text>
+              <Text style={styles.thinkText1}>해바라기를 선물로</Text>
+              <Text style={styles.thinkText2}>받아봤으면 했어요.</Text>
+              <View style={styles.line}></View>
+              <View style={styles.distance}>
+                <Image source={require('../assets/png/distance.png')} style={styles.distanceImage} />
+                <Text style={styles.GPSText}>신곡동 53-52</Text>
+              </View>
+              <View style={styles.btnInner}>
+                <View style={styles.soundContainer}>
+                  <Image source={require('../assets/png/sound.png')} style={styles.SIcon} />
+                </View>
+                <View style={styles.recorderContainer}>
+                  <Image source={require('../assets/png/recorder.png')} style={styles.RIcon} />
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.emptyContainer}></View>
+        </View>
+      </ScrollView>
+    </TouchableOpacity>
   );
 };
 
